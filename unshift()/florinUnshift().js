7ef23e5c8f7e7b0303640df3c6ba7ecccc9be4cb
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2021-03-04 13:08:00 -0800

    added unshift

---
+++ unshift()/florinUnshift().js
@@ -0,0 +1,15 @@
+//adds 1 or more elements to the beginning of an array
+//stores the number
+//opposite of the push method
+
+//ex1
+const numbers = [1, 2, 3];
+numbers.unshift(0);
+console.log(numbers); //[0,1,2,3]
+
+//ex2
+const nums = [3, 4, 5, 6];
+
+const addedNums = nums.unshift(0, 1, 2);
+
+console.log(addedNums); // 7 numbers are in the array
