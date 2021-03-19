a470602686d997d03e0c5a48b48e5314ab53f4ee
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2021-03-18 20:06:00 -0700

    updated files

--- JavaScript-Exercises/Exercises1.js
+++ JavaScript-Exercises/Exercises1.js
@@ -18,6 +18,7 @@ console.log(getFileExtension("index.html"));
 //Write a JavaScript program to replace every character
 //in a given string with the character following it in the alphabet
 
+//built in functions
 //String.fromCharCode
 //charCodeAt
 
@@ -28,3 +29,16 @@ const moveCharsForward = (str) =>
     .join("");
 
 console.log(moveCharsForward("abcd"));
+
+//Ex4
+//Write a JavaScript program to get the current date.
+//Expected: mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+const formatDate = (date = new Date()) => {
+  const days = date.getDate() + 1;
+  const months = date.getMonth() + 1;
+  const years = date.getFullYear();
+
+  return `${days}/${months}/${years}`;
+};
+console.log(formatDate());
