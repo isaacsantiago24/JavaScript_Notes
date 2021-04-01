6edd3ee37c8eaab15af707ed87ce13f426b37ea9
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2021-04-01 13:50:28 -0700

    updated files

--- JavaScript-Exercises/Exercises2.js
+++ JavaScript-Exercises/Exercises2.js
@@ -9,3 +9,34 @@ const makeNewString = (str) =>
 makeNewString("abc");
 makeNewString("abcdef");
 makeNewString("abcdsfsdf123");
+
+//write a JS program to extract the first half of a string of even length
+
+const firstHalf = (str) => str.slice(0, str.length / 2);
+console.log(firstHalf("temp"));
+console.log(firstHalf("temples"));
+console.log(firstHalf("temple"));
+
+//write a javascript program to concatenate two strings
+//except their first character
+
+const concat = (str1, str2) => str1.slice(1) + str2.slice(1);
+console.log(concat("hello", "isaac")); //ellosaac
+
+//Given two values, write a javascript program to find out which is nearest to 100
+
+const near100 = (a, b) => (100 - a < 100 - b ? a : b);
+
+console.log(near100(20, 40));
+
+//Write a JavaScript program to check if a given string contains
+//2 to 4 occurrences of a specified character.
+
+const countChars = (str, char) =>
+  str.split("").filter((ch) => ch === char).length;
+
+const contains2To4 = (str, char) =>
+  countChars(str, char) >= 2 && countChars(str, char) <= 4;
+
+console.log(contains2To4("ooh!", "o"));
+console.log(contains2To4("oh!", "o"));
